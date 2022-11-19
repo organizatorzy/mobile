9da74d11a86935e7ff9f6900fd000a172bac152f
jagtym <jagtymxd@gmail.com>
2022-11-19 22:22:16 +0100

    add photo view

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Stack } from './src/config/navigation'
 
-import { HOME_VIEW, INSTRUCTIONS_VIEW, LOGIN_VIEW, MULTI_CHOICE_VIEW, SCANNING_VIEW,GMINA_VIEW } from './src/config/paths'
+import { HOME_VIEW, INSTRUCTIONS_VIEW, LOGIN_VIEW, MULTI_CHOICE_VIEW, SCANNING_VIEW,GMINA_VIEW, PHOTO_VIEW } from './src/config/paths'
 
 
 import { HomeView } from './src/views/HomeView';
@@ -10,6 +10,7 @@ import { InstructionView } from './src/views/InstrunctionView';
 import { ScanningView } from './src/views/ScanningView';
 
 import { GminaView } from './src/views/GminaView';
+import { PhotoView } from './src/views/PhotoView';
 
 import { LoginView } from './src/views/LoginView';
 import { MultiChoiceView } from './src/views/MultiChoiceView';
@@ -26,6 +27,7 @@ export default function App() {
         <Stack.Screen name={GMINA_VIEW} component={GminaView} options={{ headerShown: false }} />
         <Stack.Screen name={LOGIN_VIEW} component={LoginView} options={{ headerShown: false }} />
         <Stack.Screen name={MULTI_CHOICE_VIEW} component={MultiChoiceView} options={{ headerShown: false }} />
+        <Stack.Screen name={PHOTO_VIEW} component={PhotoView} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
